735dfc3a1cc3da493b244d0e0bb284a33e20b2e8
tapo4ok <140212319+tapo4ok@users.noreply.github.com>
2024-07-22 10:10:06 +0200

    init

--- src/main/java/mdk/fastuixml/FastUiXML.java
+++ src/main/java/mdk/fastuixml/FastUiXML.java
@@ -1,13 +1,17 @@
 package mdk.fastuixml;
 
+import mdk.fastuixml.command.FastUIcommand;
 import mdk.fastuixml.ui.UI;
 
 import mdk.fastuixml.xml.Loader;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class FastUiXML extends JavaPlugin {
     public static final FilenameFilter filter = new FilenameFilter() {
@@ -18,8 +22,16 @@ public final class FastUiXML extends JavaPlugin {
     };
     @Override
     public void onEnable() {
+        Logger logger = getLogger();
         getServer().getPluginManager().registerEvents(new UI.EventHandler(), this);
 
+        {
+            FastUIcommand fastUIcommand = new FastUIcommand();
+            PluginCommand command = getCommand("fastui");
+
+            command.setTabCompleter(fastUIcommand);
+            command.setExecutor(fastUIcommand);
+        }
 
         Loader loader = Loader.getInstance();
         File data = getDataFolder();
@@ -32,6 +44,7 @@ public final class FastUiXML extends JavaPlugin {
             try {
                 UI ui = loader.load(Files.newInputStream(file.toPath()));
                 ui.regster(this);
+                logger.log(Level.INFO, String.format("Load ui %s", ui.getIdentifier()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
---
+++ src/main/java/mdk/fastuixml/command/FastUIcommand.java
@@ -0,0 +1,108 @@
+package mdk.fastuixml.command;
+
+import com.google.common.collect.ImmutableList;
+import mdk.fastuixml.FastUiXML;
+import mdk.fastuixml.ui.UI;
+import mdk.fastuixml.xml.Loader;
+import mdk.mutils.Identifier;
+import org.apache.commons.lang.Validate;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.StringUtil;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.logging.Level;
+
+public class FastUIcommand implements CommandExecutor, TabExecutor {
+    private final String[] w1 = new String[] {
+            "reload",
+            "open",
+            "help"
+    };
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(args.length>0)) {
+            sender.sendMessage(String.format("/%s (%s)", "fastui", String.join("/", w1)));
+            return false;
+        }
+
+        switch (args[0]) {
+            case "open": {
+                Identifier identifier = new Identifier(args[1], args[2]);
+                for (UI ui : UI.REGISTRY) {
+                    if (ui.getIdentifier().equals(identifier)) {
+                        ui.open((Player) sender);
+                    }
+                }
+                break;
+            }
+            case "reload": {
+                JavaPlugin plugin = JavaPlugin.getPlugin(FastUiXML.class);
+
+                Iterator<UI> iterator = UI.REGISTRY.iterator();
+                while (iterator.hasNext()) {
+                    iterator.remove();
+                }
+
+                Loader loader = Loader.getInstance();
+                File data = plugin.getDataFolder();
+                if (!data.exists()) data.mkdirs();
+
+                File[] uis = data.listFiles(FastUiXML.filter);
+
+                assert uis != null;
+                for (File file : uis) {
+                    try {
+                        UI ui = loader.load(Files.newInputStream(file.toPath()));
+                        ui.regster(plugin);
+                        plugin.getLogger().log(Level.INFO, String.format("Load ui %s", ui.getIdentifier()));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            }
+            case "help":
+            default:
+            {
+                sender.sendMessage("/fastui reload");
+                sender.sendMessage("/fastui help");
+                sender.sendMessage("/fastui open <namespace> <path>");
+                break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Validate.notNull(sender, "Sender cannot be null");
+        Validate.notNull(args, "Arguments cannot be null");
+        Validate.notNull(alias, "Alias cannot be null");
+
+        if (args.length == 0) {
+            return ImmutableList.of();
+        } else if (args.length == 1) {
+            String lastWord = args[args.length - 1];
+            List<String> matchedPlayers = new ArrayList();
+            Iterator<String> var7 = Arrays.stream(w1).iterator();
+            while(var7.hasNext()) {
+                String name = (String)var7.next();
+                if (StringUtil.startsWithIgnoreCase(name, lastWord)) {
+                    matchedPlayers.add(name);
+                }
+            }
+
+            Collections.sort(matchedPlayers, String.CASE_INSENSITIVE_ORDER);
+            return matchedPlayers;
+        }
+
+        return ImmutableList.of();
+    }
+}
